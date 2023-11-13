1a6d326a337bf27a2e5df445a4acede08d9adac5
zacMadison <149635415+zacMadison@users.noreply.github.com>
2023-11-13 12:13:31 -0600

    Now switches databases
    dtype error not resolved, program is horribly inefficient and takes forever to process file
    TOP PRIORITY: make it check what year the tweet was made, this database is barely organized :))))))

--- wordsAndDates.py
+++ wordsAndDates.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 
-# todo: check for all months or include user input to decide what month we are looking for, have
-# todo: the program scrape for words and place them in a list for the month they were in
+
 # seems useful ->(https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-12.php)
-# todo: also use the other databases that are not BTCP1
-# Next: Make work with more months
-# Next Next: use all databases
+# todo: look for the year it was posted because this is the worst organized database I've ever seen
+# Next: Make work with more months - done
+# Next Next: use all databases - done?
 # Next Next Next: fetch the words used in the tweets
 
 
@@ -14,18 +13,20 @@ import pandas as pd
 def findMonth():
     # set up variable to read the csvs
     filePath = 'BTCP1.csv'
-
+    currentFile = 1
     # dataFrames are used by the module pandas, the reason I chose this module over csv is that it has more functionality
-    dataFrame = pd.read_csv(filePath)
+    # attempted to solve dtype error: unsuccessful https://stackoverflow.com/questions/24251219/pandas-read-csv-low-memory-and-dtype-options
+    dataFrame = pd.read_csv(filePath, sep=',', dtype={'created_at': str})
 
     #used for loop
     completed = 0
 
     # row will be incremented through each loop
-    row = 1
+    row = 0
     date_column = 'created_at'
     while completed == 0:
-        # ISSUE: row starts at 7 then skips to 463568 and I have no clue why
+        row += 1
+
 
         print(row)
         # Note: dates are not all in order, best to continue interating loop after the month that is being looked for and find another way to end look
@@ -60,8 +61,13 @@ def findMonth():
 
         # This is for testing will be used to change files later
         if row == len(dataFrame) - 1:
-            print("reached end of file")
-        row += 1
+            row = 0
+            print("reached end of file, changing to next file")
+            filePath = "BTCP" + str(currentFile) + ".csv"
+            dataFrame = pd.read_csv(filePath, sep=',', dtype={'created_at': str})
+
+
+
 
 
 findMonth()
