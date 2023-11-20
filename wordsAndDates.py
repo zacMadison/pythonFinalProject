3e3d72ff60e45d4ffd83bfc82dac5bd51bc9d375
zacMadison <149635415+zacMadison@users.noreply.github.com>
2023-11-20 12:14:25 -0600

    Added user input, only goes through databases need to be processed
    also dtype error no longer occurs, no clue why but we take those

--- wordsAndDates.py
+++ wordsAndDates.py
@@ -1,73 +1,107 @@
 import pandas as pd
 
+programRunning = 1
+
+# Lists that contain the corresponding databases for faster processing, the user will input what month they want
+# and it will use the list to read the corresponding databases, only use may - Nov 2019
+
+
 
 
 # seems useful ->(https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-12.php)
-# todo: look for the year it was posted because this is the worst organized database I've ever seen
-# Next: Make work with more months - done
-# Next Next: use all databases - done?
-# Next Next Next: fetch the words used in the tweets
+# Next: Make fetch and process the correct databases
+# Next Next: process words in the tweets and exclude common words
+# final: output order of common words
 
 
+# recieve input from user for which month to use
+# if the database is zero program should stop running
+def recieveMonth():
+    may = [3,0]
+    jun = [3, 4, 0]
+    jul = [4, 5, 0]
+    aug = [5, 6, 0]
+    sep = [6, 7, 0]
+    oct = [7, 8, 0]
+    nov = [8, 0]
+    receivingInput = 1
+    months = [may, jun, jul, aug, sep, oct, nov]
+    while receivingInput == 1:
+        targetMonth = input("Please input a month between May and Nov by typing the first 3 letter of the month: ")
+        if targetMonth.lower() in "may":
+            targetMonth = "May"
+            databases = may
+            receivingInput = 0
+        elif targetMonth.lower() in "jun":
+            targetMonth = "Jun"
+            databases = jun
+            receivingInput = 0
+        elif targetMonth.lower() in "jul":
+            targetMonth = "Jul"
+            databases = jul
+            receivingInput = 0
+        elif targetMonth.lower() in "aug":
+            targetMonth = "Aug"
+            databases = aug
+            receivingInput = 0
+        elif targetMonth.lower() in "sep":
+            targetMonth = "Sep"
+            databases = sep
+            receivingInput = 0
+        elif targetMonth.lower() in "oct":
+            targetMonth = "Oct"
+            databases = oct
+            receivingInput = 0
+        elif targetMonth.lower() in "nov":
+            targetMonth = "Nov"
+            databases = nov
+            receivingInput = 0
+        else:
+            print("Invalid input, please try again.")
 
-def findMonth():
+    return targetMonth, databases
+
+
+def findMonth(database, targetMonth):
+    print("processing, please wait...")
     # set up variable to read the csvs
-    filePath = 'BTCP1.csv'
-    currentFile = 1
+    currentDatabase = 0
+    filePath = 'BTCP' + str(database[currentDatabase]) + '.csv'
+
     # dataFrames are used by the module pandas, the reason I chose this module over csv is that it has more functionality
     # attempted to solve dtype error: unsuccessful https://stackoverflow.com/questions/24251219/pandas-read-csv-low-memory-and-dtype-options
-    dataFrame = pd.read_csv(filePath, sep=',', dtype={'created_at': str})
+    dataFrame = pd.read_csv(filePath)
 
     #used for loop
-    completed = 0
+    completed = 1
 
     # row will be incremented through each loop
     row = 0
     date_column = 'created_at'
-    while completed == 0:
+    while completed == 1:
         row += 1
-
-
-        print(row)
         # Note: dates are not all in order, best to continue interating loop after the month that is being looked for and find another way to end look
         # also, running this also returns dtype error, doesnt seem to affect output right now
         # this will add the words in post to a list in the future
-        if "Jan" in dataFrame.at[row, date_column]:
-            print("Jan")
-        elif "Feb" in dataFrame.at[row, date_column]:
-            print("Feb")
-        elif "Mar" in dataFrame.at[row, date_column]:
-            print("Mar")
-        elif "Apr" in dataFrame.at[row, date_column]:
-            print("Apr")
-        elif "May" in dataFrame.at[row, date_column]:
-            print("May")
-        elif "Jun" in dataFrame.at[row, date_column]:
-            print("Jun")
-        elif "Jul" in dataFrame.at[row, date_column]:
-            print("Jul")
-        elif "Aug" in dataFrame.at[row, date_column]:
-            print("Aug")
-        elif "Sep" in dataFrame.at[row, date_column]:
-            print("Sep")
-        elif "Oct" in dataFrame.at[row, date_column]:
-            print("Oct")
-        elif "Nov" in dataFrame.at[row, date_column]:
-            print("Nov")
-        elif "Dec" in dataFrame.at[row, date_column]:
-            print("Dec")
+        if targetMonth in dataFrame.at[row, date_column]:
+            print("mon found")
         else:
             completed = 1
 
         # This is for testing will be used to change files later
-        if row == len(dataFrame) - 1:
+        if database[currentDatabase] == 0:
+            completed = 0
+        elif row == len(dataFrame) - 1:
             row = 0
+            currentDatabase += 1
+
+
             print("reached end of file, changing to next file")
-            filePath = "BTCP" + str(currentFile) + ".csv"
-            dataFrame = pd.read_csv(filePath, sep=',', dtype={'created_at': str})
+            filePath = "BTCP" + str(database[currentDatabase]) + ".csv"
+            dataFrame = pd.read_csv(filePath)
 
 
 
-
-
-findMonth()
+while programRunning == 1:
+    targetMonth, databases = recieveMonth()
+    findMonth(databases, targetMonth)
